c801d5f79e62184355894413cc98a9ea13590e39
Emmanuel Loku <E.loku@outlook.com>
2022-12-09 05:28:26 -0800

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - write a program that print and that piece of art is useful
+ * Return: 1
+*/
+int main(void)
+{
+	write("and that piece of arit is useful\" - Dora Korar,2015-10-19\n");
+	return(1);
+}
